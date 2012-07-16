92ccd84eea2f8a08e4af47dc46705fcf80b4c6ab
Matt Strader <mstrader@physics.ucsb.edu>
2012-07-16 15:27:59 -0700

    changed paths in start scripts and python files for dependencies (now in lib)

--- DataReadout/readout_controls/startPacketMaster.sh
+++ DataReadout/readout_controls/startPacketMaster.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-LOGNAME=logs/pm$(date +%Y%m%d-%H%M%S).log
-cp ~/Matt/DataReadout/obs_10s.h5 obs_2s.h5
+LOGNAME=~/DataReadout/logs/pm$(date +%Y%m%d-%H%M%S).log
+cp ~/Matt/DataReadout/obs_10s.h5 ~/DataReadout/obs_2s.h5
 
-h5cc -shlib -pthread -o PacketMaster PacketMaster.c
+h5cc -shlib -pthread -o ~/DataReadout/PacketMaster lib/PacketMaster.c
 #echo "PacketMaster will save logs in $LOGNAME"
-sudo nice -n -10 ./PacketMaster obs_2s.h5
+sudo nice -n -10 ~/DataReadout/PacketMaster obs_2s.h5
 
 
