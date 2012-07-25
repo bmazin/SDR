4df31bd24a5c115fe20396d13aeae5d4147841f4
Matt Strader <mstrader@physics.ucsb.edu>
2012-07-24 23:16:20 -0700

    Removed project file structure from repository, but added timing reports (.twx) and xflow logs (.log) separately

--- Firmware/projects/chan_550_simple/XPS_ROACH_base/gen_prog_files.bat
+++
@@ -1,3 +0,0 @@
-copy implementation\system.bit ..\bit_files\chan_550_simple_2012_Jul_23_1908.bit
-mkbof.exe -o implementation\system.bof -s core_info.tab -t 3 implementation\system.bin
-copy implementation\system.bof ..\bit_files\chan_550_simple_2012_Jul_23_1908.bof
