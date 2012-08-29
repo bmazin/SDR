b998e2a5d09cbe117ce0ee764286aedcc49c694a
Matt Strader <mstrader@physics.ucsb.edu>
2012-08-29 10:15:46 -0700

    Added intelligible variable names for IF programming. Untested for 512.

--- DataReadout/ChannelizerControls/lib/clock_pll_setup_512.py
+++ DataReadout/ChannelizerControls/lib/clock_pll_setup_512.py
@@ -1,9 +1,9 @@
+import fractions
 import corr, time, sys
 r = str(sys.argv[1])
 print "opening client and programming "+r+"...",
 roach = corr.katcp_wrapper.FpgaClient(r, 7147)
-#roach = corr.katcp_wrapper.FpgaClient(r+'.physics.ucsb.edu', 7147)
-#roach = corr.katcp_wrapper.FpgaClient('roach1.physics.ucsb.edu', 7147)
+
 time.sleep(1)
 roach.progdev('if_setup.bof')
 time.sleep(3)
@@ -12,15 +12,49 @@ print "done"
 
 #	Set up CK PLL
 #	Write 32 bit word to SER_DI
-print "setting up CK PLL for 512 MHz...",
-reg5 = 2**22 + 2**2 + 2**0
-reg4 = 2**2 + 2**3 + 2**4 + 2**5 + 2**16 + 2**18 + 2**20 + 2**21 + 2**23
-reg3 = 2**0 + 2**1 + 2**4 + 2**5 + 2**7 + 2**10
-reg2 = 2**27 + 2**26 + 2**14 + 2**11 + 2**10 + 2**9 + 2**7 + 2**6 + 2**1
-reg1 = 2**27 + 2**15 + 2**5 + 2**3 + 2**0
-reg0 = 2**23 + 2**22 + 2**19 + 2**18 + 2**15 + 2**4 + 2**3
-regs = [reg5, reg4, reg3, reg2, reg1, reg0]
+print "setting up CK PLL for 532 MHz...",
 
+freq=512.0e6
+sweep_freq=0
+ref_division_factor=8
+f_pfd = 10e6/ref_division_factor
+f = freq
+	
+INT = int(f)/int(f_pfd)
+MOD = 2000
+FRAC = int(round(MOD*(f/f_pfd-INT)))
+if FRAC != 0:
+	gcd = fractions.gcd(MOD,FRAC)
+	if gcd != 1:
+		MOD = MOD/gcd
+		FRAC = int(FRAC/gcd)
+
+PHASE = 1
+R = 1
+power = 3
+aux_power = 3
+MUX = 0
+LOCK_DETECT = 1
+PRESCALAR = 1 #4/5 mode f<3GHz
+CHARGE_PUMP_CURRENT_SETTING=7
+LDP=1# 6 ns
+POLARITY=1 #positive
+ENABLE_RF_OUTPUT = 1
+ENABLE_AUX_OUTPUT = 1
+BAND_SELECT_CLOCK_DIVIDER=80
+FEEDBACK_SELECT=1#fundamental
+DIVIDER_SELECT=3# div factor = 8
+CLOCK_DIVIDER_VALUE=150
+DBB=1
+CTRL_BITS = [0,1,2,3,4,5]
+reg5 = (LOCK_DETECT<<22) + CTRL_BITS[5]
+reg4 = (FEEDBACK_SELECT<<23) +(DIVIDER_SELECT<<20)+ (BAND_SELECT_CLOCK_DIVIDER<<12) + (ENABLE_AUX_OUTPUT<<8) + (aux_power<<6) + (ENABLE_RF_OUTPUT<<5) + (power<<3) + CTRL_BITS[4]
+reg3 = (CLOCK_DIVIDER_VALUE<<3) + CTRL_BITS[3]
+reg2 = (MUX<<26) + (R<<14) + (CHARGE_PUMP_CURRENT_SETTING<<9) + (LDP<<7) + (POLARITY<<6) + CTRL_BITS[2]
+reg1 = (PRESCALAR<<27) + (PHASE<<15) + (MOD<<3) + CTRL_BITS[1]
+reg0 = (INT<<15) + (FRAC<<3)+CTRL_BITS[0]
+regs = [reg5, reg4, reg3, reg2, reg1, reg0]
+print regs
 for r in regs:
 	roach.write_int('CK_SLE', 1)
 	roach.write_int('SER_DI', r)
@@ -29,6 +63,3 @@ for r in regs:
 	roach.write_int('start', 0)
 	roach.write_int('CK_SLE', 0)
 print "done"
-
-
-
