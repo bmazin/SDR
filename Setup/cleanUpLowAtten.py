81052cf64f4099ad0ac4433f38478e8fd3c0a34e
Matt Strader <mstrader@physics.ucsb.edu>
2012-09-04 11:59:07 -0700

    Run this after PSFit to take out outlier powers

---
+++ Setup/cleanUpLowAtten.py
@@ -0,0 +1,72 @@
+import os
+import numpy
+import pylab as plt
+#import matplotlib.mlab as mlab
+#import matplotlib.pyplot as plt
+
+#with open('~data/20120812adr/FL1-ps_freq0.txt', 'r') as f:
+
+path='/home/sean/data/LICK2012/20120901/'
+filename = 'ps_freq5'
+outfilename = filename + '-old'
+
+os.rename(path+filename+'.txt',path+outfilename+'.txt')
+
+numBin=20
+
+data=numpy.loadtxt(path+outfilename+'.txt')
+print data[1:,3]
+print 'median: ', numpy.median(data[1:,3])
+avg=numpy.mean(data[1:,3])
+print 'average is: ', avg
+print 'min is: ', min(data[1:,3])
+sig=numpy.std(data[1:,3])
+print 'standard deviation: ',sig
+
+h=plt.hist(data[1:,3],bins=numBin)
+
+plt.xlabel('Attenuations')
+plt.ylabel('Number')
+plt.title('title')
+
+#plt.ion()
+p=plt.show()
+
+newData=data[1:,3]
+minAtten=input('Input min Attenuation: ')
+
+for i in range(len(newData)):
+    if newData[i] < minAtten:
+        #print newData[i]
+        newData[i]=minAtten
+
+print newData
+h2=plt.hist(newData,bins=numBin)
+
+
+
+plt.show()
+#enter=input('Press any key to continue...')
+
+
+data[1:,3]=newData
+#numpy.savetxt(path+outfilename+'txt',data,delimiter='/t')
+
+f=open(path+filename+'.txt','w')
+f.write('\n\n')
+for i in range(len(data[1:,3])):
+    #f.write(str((data[i,0],'\t',data[i,1],'\t',data[i,2],'\t',data[i,3])))
+    f.write(str(data[i,0]))
+    f.write('\t')
+    f.write(str(int(data[i,1])))
+    f.write('\t')
+    f.write(str(int(data[i,2])))
+    f.write('\t')
+    f.write(str(int(data[i,3])))
+    f.write('\n')
+
+
+
+f.close()
+
+#numpy.savetxt('testReaddata.txt',[[4,2,3,4],[8,6,7,8],[29,23,25,27]],fmt=['%f','%d','%d','%d'],delimiter='\t')
