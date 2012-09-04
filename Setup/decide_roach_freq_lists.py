a509c2de5ba6a7daae1bda40b4016ddb8170ec13
Matt Strader <mstrader@physics.ucsb.edu>
2012-09-04 11:32:02 -0700

    Added manual IFs for both feedlines

--- Setup/decide_roach_freq_lists.py
+++ Setup/decide_roach_freq_lists.py
@@ -5,9 +5,9 @@ NRoaches=4 #number of roaches per feedline
 roachBandwidth=0.512 #GHz, bandwidth that each roach can cover, depends on dac clockrate
 
 #resonant frequency list for the feedline
-dir = '/home/sean/data/20120827/'
-feedline=1
-datafilename=dir+'20120823_FL%d_100mK_gold0-good.txt'%feedline
+dir = '/home/sean/data/LICK2012/20120901/'
+feedline=2
+datafilename=dir+'Lick-FL%d-good.txt'%feedline
 table = np.loadtxt(datafilename)
 
 freqs = table[:,2]
@@ -27,28 +27,32 @@ totalSpread=fLast-fStart
 #set default freq ranges for each roach or manually tweak them
 roachFreqStart = np.array([fStart]*NRoaches)
 roachLOFreq = np.array([fStart]*NRoaches)
+if feedline == 1:
+    roachLOFreq[0] = 3.6757238
+    roachLOFreq[1] = 4.3383665
+    roachLOFreq[2] = 4.9063277
+    roachLOFreq[3] = 5.5724130
+else:
+    roachLOFreq[0] = 3.6536534
+    roachLOFreq[1] = 4.3752050
+    roachLOFreq[2] = 4.9094138
+    roachLOFreq[3] = 5.6137981
+
+    
 #roachFreqStart[0] = fStart
-roachLOFreq[0] = 3.7140334
 #roachFreqStart[1] = roachFreqStart[0]+roachBandwidth
 #roachFreqStart[1] = roachFreqStart[0]+roachBandwidth+.05
-roachLOFreq[1] = 4.2943301
 #roachFreqStart[2] = roachFreqStart[1]+roachBandwidth+.01
-roachLOFreq[2] = 4.8514509
-
 #roachFreqStart[3] = roachFreqStart[2]+roachBandwidth+.05
-roachLOFreq[3] = 5.6225290
 
 roachFreqEnd = roachLOFreq+roachBandwidth/2.0
 roachFreqStart = roachLOFreq-roachBandwidth/2.0
-print roachFreqStart
-print roachFreqEnd
-
 
 roachFreqList=[[]]*NRoaches
 outputTable=[[]]*NRoaches
 excludedFreqs = freqs
 
-lo_fid=open(dir+'FL%d-lofreqs-again.txt'%feedline,'w')
+lo_fid=open(dir+'FL%d-lofreqs.txt'%feedline,'w')
 for iRoach in np.arange(0,NRoaches):
     r = iRoach+4*(feedline-1)
     roachFreqList[iRoach] = freqs[np.logical_and(roachFreqStart[iRoach] <= freqs,freqs < roachFreqEnd[iRoach])]
@@ -61,7 +65,7 @@ for iRoach in np.arange(0,NRoaches):
     print 'with LO freq at %.3f'%roachLOFreq[iRoach]
     lo_fid.write('%.5f\n'%roachLOFreq[iRoach])
     ax.hist(roachFreqList[iRoach],bins=20)
-    fid = open(dir+'FL%d-freq%d-again.txt'%(feedline,r),'w')
+    fid = open(dir+'freq%d.txt'%(feedline,r),'w')
     fid.write('1\t1\t1\t1\n')
     for freq in roachFreqList[iRoach]:
         fid.write('%.7f\t0\t0\t1\n'%freq)
