c06e4fa292aec83cbbf8e7655b7bd01978407107
Matt Strader <mstrader@physics.ucsb.edu>
2012-09-17 19:22:23 -0700

    Use hexlify. Has a bug

--- Projects/OptimalFilterTimestream/qdr_read.py
+++ Projects/OptimalFilterTimestream/qdr_read.py
@@ -3,24 +3,31 @@ import corr
 import time
 import struct
 import matplotlib.pyplot as plt
+import binascii
 
 
-roach = corr.katcp_wrapper.FpgaClient('10.0.0.11',7147)
+roach = corr.katcp_wrapper.FpgaClient('10.0.0.10',7147)
 time.sleep(2)
 print 'connection established'
 
+wordSize = 32
 L=2**20
+nWords = L*8/wordSize
+
+print '\n'
+#start firmware writing to qdr
 roach.write_int('startSnap',0)
 time.sleep(0.1)
 roach.write_int('startSnap',1)
-time.sleep(5)
-data = roach.read('qdr0_memory',4*L)
-time.sleep(0.1)
+time.sleep(0.01)
 roach.write_int('startSnap',0)
-phase = struct.unpack('>%dI'%L,data)
-
-
-fig=plt.figure()
-ax=fig.add_subplot(111)
-ax.plot(np.arange(len(phase)),phase)
-plt.show()
+time.sleep(5)
+#read out what was written
+data = roach.read('qdr0_memory',L)
+#convert to hex and then to binary strings
+binstr = bin(int(binascii.hexlify(data),16))[2:]
+#print in groups of 32 bits, with four groups in a row
+for i in range(nWords):
+    print binstr[i*wordSize:i*wordSize+wordSize],
+    if i%4 == 3:
+        print ''
