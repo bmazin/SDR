c755a24b8af14ff235c6759d5fd9b4efcdf2e1ea
Matt Strader <mstrader@physics.ucsb.edu>
2012-09-17 18:37:45 -0700

    Changed default firmware to one with fixed baseline triggers

--- DataReadout/ChannelizerControls/startFirmware.sh
+++ DataReadout/ChannelizerControls/startFirmware.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-ROACHES=(0 1 2 3 4 5 6 7)
+ROACHES=(2 3 4 5 6 7)
 CLK="512"
-BOF="chan_dtrig_v2_2012_Aug_28_1956.bof"
+#BOF="chan_dtrig_v2_2012_Aug_28_1956.bof"
 #BOF="chan_if_acc_x_2011_Aug_02_0713.bof"
+BOF="btrig_fix_v1_2012_Sep_06_1516.bof"
 
 if [ "$1" == "--help" ]; then
 	echo "Usage: $0 CLOCK_MHZ [BOF_FILE]"
