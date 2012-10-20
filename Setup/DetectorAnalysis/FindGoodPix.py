Latest bug fixes (infrared) and cleaner qe formula

Setup/DetectorAnalysis/FindGoodPix.py
@@ -5,22 +5,30 @@ import math
 
 #This program evaluates the functuality of each pixel in a single roach board.  Alter the time range to match a peak (look at qeroughplot.py to visualize the peaks).  The output is a sorted list of [average # of photons seen, pixel] View qe plots of the best pixels using qeplotforpixels.py
 
-qeDataFilename= '/media/disk2/sci3gamma/qe_20120824-230828.h5' 
-qeObsTime=1345874908
-
-roach=4
+#qeDataFilename= '/home/sean/data/20121010/obs_20121010-215637.h5'
+qeDataFilename= '/home/sean/data/20121018/obs_20121019-044657.h5'
+#qeObsTime=1349906199
+qeObsTime=1350622020
+roach=5
 
 
 NPixels=253
 
 finish=[]
+Final=[]
+pixels=[]
 fid = tables.openFile(qeDataFilename,mode='r')
 
 for pix in xrange(0,NPixels):
 	temp=[]
 	dataset=fid.getNode('/r%d/p%d/t%d'%(roach,pix,qeObsTime))
-	for t in xrange(260,270):	
+	for t in xrange(316,324):	
 		temp.append(len(dataset[t])) 
 	finish.append([int(np.average(temp)),pix])
-print sorted(finish)
+Final=sorted(finish)
+print Final
+for pix in xrange(0,len(Final)):
+    Final[pix]=Final[pix][1]
+
+print Final
 fid.close()

Setup/DetectorAnalysis/QPixelQuality.py
@@ -11,15 +11,17 @@ from PixelQualityfunc import *
 
 closefcutoff=.0007   #when the program compares the approximate freq in frequency files to the accurate frequency in the fit file, it will accept a difference of the closefcutoff (GHz)
 
-bmaptitle='/media/disk2/sci3gamma/20120904/PositionKnownOnlybeamimage_double_refined_20120904.h5'
+#bmaptitle='/media/disk2/sci3gamma/20120904/PositionKnownOnlybeamimage_double_refined_20120904.h5'
+bmaptitle='/home/sean/data/20121010/beamimage_sci4_20121010.h5'
 
 #for feedline 1
-title1=title2='20120827/ps_freq'
-titlefits1='20120823adr/20120823_FL1_100mK_gold0-fits.txt'
+title1=title2='20121006/ps_freq'
+titlefits1='20121006/20121006-SCI4-ADR-FL1-fits.txt'
 #for feedline 2
-titlefits2='20120823adr/20120823_FL2_100mK_gold0-fits.txt'
+titlefits2='20121006/20121006-SCI4-ADR-FL2-fits.txt'
 
-title='/home/sean/data/fitshist/20120912QPixelQuality.fits'
+outTitle='/home/sean/data/fitshist/20121011_Qi_PixelQuality.fits'
+freqOutTitle='/home/sean/data/fitshist/20121011_freqMap.fits'
  
 
 #--Get an accurate array with roach,pixel,f,Q in the correct position--
@@ -31,6 +33,7 @@ b=fid.root.beammap.beamimage
 
 
 pixarray = [[-1 for col in range(44)] for row in range(46)]
+freqarray = [[-1 for col in range(44)] for row in range(46)]
 pixlist = []
 for row in xrange(0,46):
     for column in xrange(0,44):
@@ -42,6 +45,7 @@ for row in xrange(0,46):
             if roachNum <8:
                 if pixelNum<len(accuratefQ[roachNum]):
     	            pixarray[row][column]=(accuratefQ[roachNum][pixelNum][1]/1000)
+    	            freqarray[row][column]=(accuratefQ[roachNum][pixelNum][0]/1000)
 
 #            else:
 #               print roachNum,pixelNum
@@ -52,7 +56,10 @@ fid.close()
 
 #--Plot what the detector looks like with the f,Q, and position written inside each 'pixel'--
 pixarray=np.array(pixarray)
+freqarray=np.array(freqarray)
 
 
 hdu = pyfits.PrimaryHDU(pixarray)
-hdu.writeto(title,clobber=True)
+hdu2 = pyfits.PrimaryHDU(freqarray)
+hdu.writeto(outTitle,clobber=True)
+hdu2.writeto(freqOutTitle,clobber=True)

Setup/DetectorAnalysis/qeforpixelsfunc.py
@@ -84,11 +84,11 @@ def NumPhotonInWVLPulses(qeDataFilename, qeObsTime, roach, pixlist, initial, pea
         tpeak=[]
         for i in xrange(0,len(peaks)/2):
             pk=np.average(photonSec[peaks[2*i]:peaks[2*i+1]])
-            #		dk=np.average(photonSec[darkflux[2*i]:darkflux[2*i+1]]+photonSec[darkflux[2*i+2]:darkflux[2*i+3]])
-            #		net=pk-dk
-            #		fin.append(net)
+            dk=np.average(photonSec[darkflux[2*i]:darkflux[2*i+1]]+photonSec[darkflux[2*i+2]:darkflux[2*i+3]])
+            net=pk-dk
+            fin.append(net)
             tpeak.append(np.average([peaks[2*i],peaks[2*i+1]]))
-            fin.append(pk)
+#            fin.append(pk)
         NPulses.append(fin)
 
         # photonSec has the total number of photons per second, 
@@ -105,9 +105,6 @@ def NumPhotonInWVLPulses(qeDataFilename, qeObsTime, roach, pixlist, initial, pea
             plt.grid(True)
             plt.show()
             plt.close()
-
-
-
 
     fid.close()
     return NPulses

Setup/DetectorAnalysis/qeplotforpixels.py
@@ -9,40 +9,55 @@ from qeforpixelsfunc import *
 #Use qeroughplot.py to figure out peakw,troughw, and ini. dectv is the values (x10^7) seen in the comparison detector (not arcons).
 
 qeDataFilename= '/home/sean/data/20121010/obs_20121010-215637.h5'
+#qeDataFilename= '/home/sean/data/20121018/obs_20121019-044657.h5'
 qeObsTime=1349906199
+#qeObsTime=1350622020
 roach=5
+
 
 #pixlist=[11, 133, 21, 109, 31, 57, 92, 3, 129, 117, 96, 16, 105, 37, 25, 102, 75, 123, 124, 38, 46, 113, 150, 70, 149, 103, 28, 34, 120, 99, 49, 93, 104, 55, 134, 80, 119, 95, 24, 64, 82, 61, 60, 138, 53, 8, 89, 141, 27, 91, 130, 54, 145, 30, 45, 97, 32, 108, 106, 146, 72, 139, 116, 77, 94, 140, 58, 107, 66, 112, 73, 41, 48, 122, 84, 128, 148, 78, 20, 36, 127, 137, 87, 76, 23, 35, 83, 98, 39, 63, 68, 115, 135, 67, 52, 71, 26, 79, 85, 59, 151, 6, 22, 100, 88, 118, 143, 111, 19, 125, 81, 50]
 pixlist=[100,115,84,57]
+#pixlist=[148,24,85,130,26]
 
-initial=100
+initial=109
+#initial=104
 peakw=14.6
+#peakw=14.6
 troughw=54
+#troughw=54
 
 #saveFilename='data/qePlots/qe20120824-r7.pdf'
 #dectv=[0.149,.497,1.156,2.305,3.820,4.762,5.014,5.746,5.232,5.056,6.466,8.5,10.513,11.783]
 #wvl=range(400,1100,50)
 testbedFile = np.loadtxt('/home/sean/data/QEmeas/SCI4-nomicrolens-1.txt')
+#testbedFile = np.loadtxt('/home/sean/data/QEmeas/SCI4-microlens-20121019-044657.txt')
 wvl = testbedFile[:,0]
 dectv=testbedFile[:,-1]
 
 NSec=1800
 
 NPulses = NumPhotonInWVLPulses(qeDataFilename, qeObsTime, roach, pixlist, initial, peakw, troughw,  dectv, NSec, nwvl=len(wvl), plotYorN = 'Y', pause= 'N', pauseInitial=0, pausePeak=1)
-
+print NPulses[0]
 fig=plt.figure()
 ax=fig.add_subplot(111)
+
+noMicrolensFactor = 30
+#noMicrolensFactor = 1
+magnification = 1.2
+#magnification = 1.45
+areaARCONS = 222*10**-6
+areaDetector = 1*10**-2
 
 qeresults=[]
 medianQeResults=[]
 for j in xrange(0,len(pixlist)):
     qepix=[]
     for i in xrange(0,len(dectv)):
-        qewvl=1/(dectv[i]*10**7/NPulses[j][i]*.0007096896)*30
+        qewvl=NPulses[j][i]/(dectv[i]*10**7)*areaDetector**2/(magnification*areaARCONS)**2*noMicrolensFactor
         qepix.append(qewvl)
     ax.plot(wvl,qepix,label='r%dp%d'%(roach,pixlist[j]))
     qeresults.append(qepix)
-qeresults = np.array(qeresults)*31   #the 31 is due to not having the microlens in place
+qeresults = np.array(qeresults)
 for wl in xrange(0,len(qeresults[0])):
     medianQeResults.append(np.median(qeresults[:,wl]))
 print medianQeResults

Setup/DetectorAnalysis/test.py
@@ -1,41 +1,17 @@
-import numpy as np
-import scipy
-
-sqDimOpt=10
-
-binaryPixArray=scipy.random.random((46,44))
-binaryPixArray=np.around(binaryPixArray)
+[50.857142857142854, 62.714285714285715, 81.214285714285708, 134.0, 159.64285714285714, 130.14285714285714, 117.21428571428571, 154.28571428571428, 217.57142857142858, 330.5, 634.71428571428567, 1017.9285714285714, 1155.1428571428571, 1648.8571428571429]
 
 
-yvariation=int((46-sqDimOpt)/2)
-xvariation=int((44-sqDimOpt)/2)
 
-binaryPixArray=np.array(binaryPixArray)
-impSection=binaryPixArray[yvariation:(46-yvariation),xvariation:(44-xvariation)]
-percentgood=np.sum(impSection)/(sqDimOpt**2.)
+[[   5.80879282    4.4622528     2.24517554    1.1967021     1.01152459
+     1.07387101    0.96170616    0.74736926    0.54616567    0.44410582
+     0.42395142    0.34001022    0.27484053    0.27820316]
 
-resultPercent=[]
-for j in xrange(-xvariation,xvariation+1):
-    for i in xrange(-yvariation, yvariation+1):
-        ditherArray=np.array([[None for col in range(sqDimOpt)] for row in range(sqDimOpt)])
-        for row in xrange(0, sqDimOpt):
-            for column in xrange(0, sqDimOpt):
-                ditherArray[row,column]=binaryPixArray[row+yvariation,column+xvariation]+binaryPixArray[(row+yvariation),(column+xvariation+j)]+binaryPixArray[(row+yvariation+i),(column+xvariation)]+binaryPixArray[(row+yvariation+i),(column+xvariation+j)]
-        resultPercent.append([sum(sum(1 for i in row if i>0) for row in ditherArray)/(sqDimOpt**2.),i,j])
-resultPercent=sorted(resultPercent)
+        noMicrolensFactor = 30
+        magnification = 1.2
+        areaARCONS = 222*10**-6
+        areaDetector = 1*10**-2
+        noMicroareaARCONS=40*10**-6
 
-i=resultPercent[len(resultPercent)-1][1]
-j=resultPercent[len(resultPercent)-1][2]
-ditherArray=np.array([[None for col in range(sqDimOpt)] for row in range(sqDimOpt)])
-for row in xrange(0, sqDimOpt):
-    for column in xrange(0, sqDimOpt):
-        ditherArray[row,column]=binaryPixArray[row+yvariation,column+xvariation]+binaryPixArray[(row+yvariation),(column+xvariation+j)]+binaryPixArray[(row+yvariation+i),(column+xvariation)]+binaryPixArray[(row+yvariation+i),(column+xvariation+j)]
-finalresultPercent=sum(sum(1 for i in row if i>0) for row in ditherArray)/(sqDimOpt**2.)
-print impSection
-print ditherArray
+                qewvl=(NPulses[j][i]*areaDetector**2)/((dectv[i]*10**7)*(magnification*noMicroareaARCONS)**2)
 
-print 'Considering the middle %dx%d pixels'%(impSection.shape[0],impSection.shape[1])
-print 'The percent of working pixels initially is %f'%percentgood
-print 'The percent with 4 dithers in rect. formation is %f'%finalresultPercent
-print 'best dither position: up %d rows and %d columns to the right'%(i,j)
-
+[5.8087928153717634, 4.4622527972937815, 2.2451755383682142, 0.71189741328630241, 0.62366035221874638, 1.0738710091134047, 0.96170616167071776, 0.74736926020408179, 0.80777292360939013, 0.78608746130030993, 0.42395141728590485, 0.26733664163679949, 0.20747298663645561, 0.21917356683771322]