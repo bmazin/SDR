567e371d4321d6c7fcabe3a21fccf3ef52164483
Matt Strader <mstrader@physics.ucsb.edu>
2012-10-22 16:58:51 -0700

    Added possible data file and moved main variables to top

--- Setup/DetectorAnalysis/MasterQE.py
+++ Setup/DetectorAnalysis/MasterQE.py
@@ -5,20 +5,18 @@ import math
 import itertools
 from qeforpixelsfunc import *
 
-savedValues= raw_input('Do you want to used the saved values? Y or enter : ')
+#savedValues= raw_input('Do you want to used the saved values? Y or enter : ')
 roach= input('Which roach do you want to analyze? ') 
 
 #qeDataFilename= '/home/sean/data/20121003/obs_20121003-210815.h5'
 #qeObsTime=1349298497
 #testbedFileName = '/home/sean/data/QEmeas/20121003_210815_postLick_sci3gamma.txt'
 #NSec=1800
-#not actually a qe file...
 
 #qeDataFilename= '/home/sean/data/20121003/obs_20121003-223105.h5'
 #qeObsTime=1349303467
 #testbedFileName = '/home/sean/data/QEmeas/20121003_223105_postLick_sci3gamma.txt'
 #NSec=2000
-#not actually a qe file...
 
 #qeDataFilename= '/home/sean/data/20121018/obs_20121019-044657.h5'
 #qeObsTime=1350622020
@@ -37,15 +35,30 @@ roach= input('Which roach do you want to analyze? ')
 #NSec=1800
 #dercutoff=10000, pauseafterpeak 14, firstpeakafterpause=1178, firstpeak=92, time interval 850-860
 
-qeDataFilename= '/home/sean/data/20121019/obs_20121019-193507.h5'
-qeObsTime=1350675309
-testbedFileName='/home/sean/data/QEmeas/SCI4-microlens-20121019-3.txt'
-NSec=1900
-##dercutoff=14000, pauseafterpeak 14, firstpeakafterpause=1178, firstpeak=86, time interval 430-440, range 350-1400
+#qeDataFilename= '/home/sean/data/20121019/obs_20121019-193507.h5'
+#qeObsTime=1350675309
+#testbedFileName='/home/sean/data/QEmeas/SCI4-microlens-20121019-3.txt'
+#NSec=1900
+##r0,dercutoff=14000, pauseafterpeak 14, firstpeakafterpause=1178, firstpeak=86, time interval 430-440, range 350-1400
 
-saveMedQEPlotname='/home/sean/data/qePlots/qe20121003-223105-r%dIRfixedLickMagmean.pdf'%(roach)
+qeDataFilename= '/home/sean/data/20121022/obs_20121022-210829.h5'
+qeObsTime=1350940111
+testbedFileName='/home/sean/data/QEmeas/SCI4-microlens-20121022-210829.txt'
+NSec=2000
+#dercutoff=9000, pauseafterpeak 14, firstpeakafterpause=1449, firstpeak=104, time interval 696-712, range 500-600
+
+saveMedQEPlotname='/home/sean/data/qePlots/qe20121022r%d.pdf'%(roach)
 
 NPixels=253
+magnificationLick = 1.2
+magnificationPalomar = 1.46
+areaARCONSwithMicrolens = 222*10**-6
+areaDetector = 1*10**-2
+IRAreaDetector = 3*10**-3
+areaARCONSwithoutMicrolens=40*10**-6
+
+areaARCONS = areaARCONSwithMicrolens
+magnification = magnificationPalomar
  
 #dectvManual=[0.149,.497,1.156,2.305,3.820,4.762,5.014,5.746,5.232,5.056,6.466,8.5,10.513,11.783]
 #wvlManual=range(400,1100,50)
@@ -264,19 +277,12 @@ if status not in ('Q', 'q', "'Q'", "'q'"):
         fig2=plt.figure()
         ax2=fig2.add_subplot(111)
 
-        magnification = 1.2
-        #magnification = 1.46
-        areaARCONS = 222*10**-6
-        areaDetector = 1*10**-2
-        IRAreaDetector = 3*10**-3
-        noMicroareaARCONS=40*10**-6
 
         qeresults=[]
         medianQeResults=[]
         meanQeResults=[]
-        print dectv
-        print dectv[0]
-        print NPulses[0][0]
+        print 'dectv=',dectv
+        print 'NPulses[0]=',NPulses[0][:]
         for j in xrange(0,len(pixlist)):
             qepix=[]
             for i in xrange(0,len(dectv)):
@@ -290,11 +296,11 @@ if status not in ('Q', 'q', "'Q'", "'q'"):
             ax2.plot(wvl,qepix,label='r%dp%d'%(roach,pixlist[j]))
             qeresults.append(qepix)
         qeresults = np.array(qeresults)
-        print qeresults[0]
+        print 'qeresult[0]=',qeresults[0]
         for wl in xrange(0,len(qeresults[0])):
             medianQeResults.append(np.median(qeresults[:,wl]))
             meanQeResults.append(np.mean(qeresults[:,wl]))
-        #print medianQeResults
+        print 'medianQE=',medianQeResults
         #print meanQeResults
 #-------Some error above.  inputs right, formula right, output not right...------
         ax2.set_xlabel('Frequency (nm)')
