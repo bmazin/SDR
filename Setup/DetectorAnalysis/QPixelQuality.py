d0fbe7d217c96334b9dc71b3ba340ca6dec7e16c
Matt Strader <mstrader@physics.ucsb.edu>
2012-10-22 19:32:53 -0700

    Restoring latest version

--- Setup/DetectorAnalysis/QPixelQuality.py
+++ Setup/DetectorAnalysis/QPixelQuality.py
@@ -6,19 +6,22 @@ import tables
 import re
 import collections
 import scipy
+import pyfits
 from PixelQualityfunc import *
 
 closefcutoff=.0007   #when the program compares the approximate freq in frequency files to the accurate frequency in the fit file, it will accept a difference of the closefcutoff (GHz)
 
-bmaptitle='/media/disk2/sci3gamma/20120904/PositionKnownOnlybeamimage_double_refined_20120904.h5'
+#bmaptitle='/media/disk2/sci3gamma/20120904/PositionKnownOnlybeamimage_double_refined_20120904.h5'
+bmaptitle='/home/sean/data/20121010/beamimage_sci4_20121010.h5'
 
 #for feedline 1
-title1=title2='20120827/ps_freq'
-titlefits1='20120823adr/20120823_FL1_100mK_gold0-fits.txt'
+title1=title2='20121006/ps_freq'
+titlefits1='20121006/20121006-SCI4-ADR-FL1-fits.txt'
 #for feedline 2
-titlefits2='20120823adr/20120823_FL2_100mK_gold0-fits.txt'
+titlefits2='20121006/20121006-SCI4-ADR-FL2-fits.txt'
 
-pdftitle='/home/sean/data/fitshist/20120912QPixelQuality.pdf'
+outTitle='/home/sean/data/fitshist/20121011_Qi_PixelQuality.fits'
+freqOutTitle='/home/sean/data/fitshist/20121011_freqMap.fits'
  
 
 #--Get an accurate array with roach,pixel,f,Q in the correct position--
@@ -29,7 +32,8 @@ fid=tables.openFile(bmaptitle)
 b=fid.root.beammap.beamimage
 
 
-pixarray = [[None for col in range(44)] for row in range(46)]
+pixarray = [[-1 for col in range(44)] for row in range(46)]
+freqarray = [[-1 for col in range(44)] for row in range(46)]
 pixlist = []
 for row in xrange(0,46):
     for column in xrange(0,44):
@@ -40,8 +44,9 @@ for row in xrange(0,46):
             pixelNum=int(a[1])
             if roachNum <8:
                 if pixelNum<len(accuratefQ[roachNum]):
-    	            pixarray[row][column]=([[roachNum,pixelNum],accuratefQ[roachNum][pixelNum]])
-                    pixlist.append([[roachNum,pixelNum],accuratefQ[roachNum][pixelNum]])
+    	            pixarray[row][column]=(accuratefQ[roachNum][pixelNum][1]/1000)
+    	            freqarray[row][column]=(accuratefQ[roachNum][pixelNum][0]/1000)
+
 #            else:
 #               print roachNum,pixelNum
 #        else:
@@ -50,49 +55,11 @@ for row in xrange(0,46):
 fid.close()
 
 #--Plot what the detector looks like with the f,Q, and position written inside each 'pixel'--
+pixarray=np.array(pixarray)
+freqarray=np.array(freqarray)
 
-fig=plt.figure(figsize=(23,14))
-plt.subplots_adjust(left = 0.04, right= 0.97, bottom= .05, top= .95)
-ax=fig.add_subplot(111)
 
-binaryPixArray = scipy.zeros((46,44))
-#binaryPixArray=scipy.random.random((46,44))
-#binaryPixArray=np.around(binaryPixArray)
-
-textsize=4.5
-problem=[]
-for row in xrange(0,46):
-    for column in xrange(0,44):
-        pix=pixarray[row][column]
-        if pix==None:
-            color='Black'
-            ax.text(.002+ 0.02275*column,0.003+ 0.0217*row, '%s\n%s\n| (%d,%d)-'%('|       ','|      ',row,column), bbox=dict(facecolor=color, alpha=.6), size=textsize)
-	elif pix[1][1]<40000:
-            binaryPixArray[row][column]=1
-            color= 'Blue'
-            ax.text(.002+ 0.02275*column,0.003+ 0.0217*row, '%s\n%.1f\n(%d,%d)'%('|         ',pix[1][1]/1000,row,column), bbox=dict(facecolor=color, alpha=.6), size=textsize)
-	elif pix[1][1]<80000:
-            binaryPixArray[row][column]=1
-            color= 'Green'
-            ax.text(.002+ 0.02275*column,0.003+ 0.0217*row, '%s\n%.1f\n(%d,%d)'%('|         ',pix[1][1]/1000,row,column), bbox=dict(facecolor=color, alpha=.6), size=textsize)
-	elif pix[1][1]<120000:
-            binaryPixArray[row][column]=1
-            color= 'Yellow'
-            ax.text(.002+ 0.02275*column,0.003+ 0.0217*row, '%s\n%.1f\n(%d,%d)'%('|         ',pix[1][1]/1000,row,column), bbox=dict(facecolor=color, alpha=.6), size=textsize)
-	elif pix[1][1]<160000:
-            binaryPixArray[row][column]=1
-            color= 'Orange'
-            ax.text(.002+ 0.02275*column,0.003+ 0.0217*row, '%s\n%.1f\n(%d,%d)'%('|         ',pix[1][1]/1000,row,column), bbox=dict(facecolor=color, alpha=.6), size=textsize)
-	else :
-            binaryPixArray[row][column]=1
-            color= 'Red'
-            ax.text(.002+ 0.02275*column,0.003+ 0.0217*row, '%s\n%.1f\n(%d,%d)'%('|         ',pix[1][1]/1000,row,column), bbox=dict(facecolor=color, alpha=.6), size=textsize)
- 
-
-#print problem	
-
-ax.text(0, 1.02, 'Each box represents a pixel. Black means the pixel is dead.\nB: Q<40k, G: 40k<Q<80k, Y: 80k<Q<120k, O: 120k<Q<160k, R: Q>160k', bbox=dict(facecolor='white', alpha=1), size=10)
-plt.title('Pixel Quality and Characteristics for 20120912 Detector', size=24, position=(.5,1.01))
-fig.savefig(pdftitle)
-plt.show()
-plt.close()
+hdu = pyfits.PrimaryHDU(pixarray)
+hdu2 = pyfits.PrimaryHDU(freqarray)
+hdu.writeto(outTitle,clobber=True)
+hdu2.writeto(freqOutTitle,clobber=True)
