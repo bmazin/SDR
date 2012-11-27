d66dad95b35173553680340c315a6e24b7e527ee
Matt Strader <mstrader@physics.ucsb.edu>
2012-11-26 21:22:39 -0800

    turn on interactive mode to automatically skip graphs

--- Setup/DetectorAnalysis/MasterQE.py
+++ Setup/DetectorAnalysis/MasterQE.py
@@ -6,6 +6,14 @@ import itertools
 from qeforpixelsfunc import *
 
 #savedValues= raw_input('Do you want to used the saved values? Y or enter : ')
+automated = raw_input('Is this input from a file?')
+if automated in ['y','Y']:
+    plt.ion()
+qeDataFilename = input('Which obs file do you want?')
+fid = tables.openFile(qeDataFilename,mode='r')
+testbedFileName = input('Which testbed file do you want?')
+testbedFile = np.loadtxt(testbedFileName)
+qeObsTime = input('What are the datasets named (obs time)?')
 roach= input('Which roach do you want to analyze? ') 
 
 #qeDataFilename= '/home/sean/data/20121003/obs_20121003-210815.h5'
@@ -29,6 +37,7 @@ roach= input('Which roach do you want to analyze? ')
 #NSec=1300
 #dercutoff=12000, firstpeak=109, time interval 800-810
 
+#SCI4 alpha, no microlens
 #qeDataFilename= '/home/sean/data/20121011/qe_20121011-185036.h5'
 #qeObsTime=1349981438
 #testbedFileName='/home/sean/data/QEmeas/SCI4-nomicrolens-2.txt'
@@ -41,13 +50,41 @@ roach= input('Which roach do you want to analyze? ')
 #NSec=1900
 ##r0,dercutoff=14000, pauseafterpeak 14, firstpeakafterpause=1178, firstpeak=86, time interval 430-440, range 350-1400
 
-qeDataFilename= '/home/sean/data/20121022/obs_20121022-210829.h5'
-qeObsTime=1350940111
-testbedFileName='/home/sean/data/QEmeas/SCI4-microlens-20121022-210829.txt'
-NSec=2000
-#dercutoff=9000, pauseafterpeak 14, firstpeakafterpause=1449, firstpeak=104, time interval 696-712, range 500-600
+#qeDataFilename= '/home/sean/data/20121022/obs_20121022-210829.h5'
+#qeObsTime=1350940111
+#testbedFileName='/home/sean/data/QEmeas/SCI4-microlens-20121022-210829.txt'
+#NSec=2000
+##dercutoff=9000, pauseafterpeak 14, firstpeakafterpause=1449, firstpeak=104, time interval 696-712, range 500-600
 
-saveMedQEPlotname='/home/sean/data/qePlots/qe20121022r%d.pdf'%(roach)
+#Sci4 alpha,best aligned microlens
+#qeDataFilename= '/home/sean/data/20121027/obs_20121028-032905.h5'
+#qeObsTime=1351394947
+#testbedFileName='/home/sean/data/QEmeas/sci4-newalignment-20121028-032905.txt'
+#NSec=2000
+##dercutoff=10000, pauseafterpeak 14, firstpeakafterpause=1431, firstpeak=86, time interval 1103-1118, range 500-600
+#saveMedQEPlotname='/home/sean/data/qePlots/qe20121027r%d.pdf'%(roach)
+
+#qeDataFilename= '/home/sean/data/20121120/obs_20121120-213604.h5'
+#qeObsTime=1353447366
+#testbedFileName='/home/sean/data/QEmeas/sci4alpha-left-2.txt'
+#NSec=2000
+##dercutoff=10000, pauseafterpeak 14, firstpeakafterpause=1439, firstpeak=96, time interval 1117-1123, range 500-600
+#saveMedQEPlotname='/home/sean/data/qePlots/qe20121120r%d.pdf'%(roach)
+#
+#qeDataFilename= '/home/sean/data/20121121/obs_20121121-230002.h5'
+#qeObsTime=1353538804
+#testbedFileName='/home/sean/data/QEmeas/sci4alpha-left-align-1.txt'
+#NSec=2000
+##dercutoff=20000,15000, pauseafterpeak 14, firstpeakafterpause=1414, firstpeak=70, time interval 665-679,
+#saveMedQEPlotname='/home/sean/data/qePlots/qe20121121r%d.pdf'%(roach)
+##Sci4a
+#qeDataFilename= '/home/sean/data/20121107/obs_20121108-015713.h5'
+#qeObsTime=1352339835
+#testbedFileName='/home/sean/data/QEmeas/sci4a-waferedge.txt'
+#NSec=2000
+## roach 0,dercutoff=9000, pauseafterpeak 14, firstpeakafterpause=1456, firstpeak=116, time interval 705-722, range 500-600
+#saveMedQEPlotname='/home/sean/data/qePlots/qe20121108r%d.pdf'%(roach)
+
 
 NPixels=253
 magnificationLick = 1.2
@@ -80,7 +117,7 @@ photonSec=[]
 
 #pulseMask = int(12*'1',2)#bitmask of 12 ones
 
-fid = tables.openFile(qeDataFilename,mode='r')
+NSec = fid.root.header.header.read()[0]['exptime']
 pulseh=[[] for x in xrange(NSec)]
 
 
@@ -108,15 +145,8 @@ for t in pulseh:
 # photonSec has the total number of photons per second, 
 print 'Things to record when you view the following plot: \n	time interval during a strong peak\n	time the first peak starts\n	if there is a pause and if so,\n		after what peak does the pause occur\n		the time the first peak after the pause starts'
 
-fig0=plt.figure()
-ax0=fig0.add_subplot(111)
-ax0.plot(photonSec)
-ax0.set_xlabel('Time')
-ax0.set_ylabel('Number of Photons')
-ax0.set_title('# Photons vs. Time')
-ax0.grid(True)
-plt.show()
-plt.close()
+#plt.ion()
+
 
 der=[]
    
@@ -124,24 +154,34 @@ for t in xrange(0, len(photonSec)-1):
    der.append(photonSec[t+1]-photonSec[t])
 
 status='R'
+riseCutoff = 10000
+fallCutoff = 10000
 
-while status in ('R', "'R'", 'r', "'r'"):
-    ncutoff= int(raw_input('What value of the derivative should define a pulse? 10000? '))
 
-    x=[1,NSec/2,NSec]
-    y1=[ncutoff,ncutoff,ncutoff]
-    y2=[-1*ncutoff,-1*ncutoff,-1*ncutoff]
-    fig1=plt.figure()
-    ax1=fig1.add_subplot(111)
-    ax1.plot(der, 'k', x,y1,'b',x,y2,'b')
-    ax1.set_xlabel('Time')
-    ax1.set_ylabel('Derivative of the Number of Photons')
-    ax1.set_title('Derivative of # Photons vs. Time')
-    ax1.grid(True)
-    plt.show()
-    plt.close()
-    status= raw_input("What do you want to do? \nRepick cutoff= R, Quit= Q, or press enter to move on   ")
+x=[1,NSec/2,NSec]
+y1=[riseCutoff,riseCutoff,riseCutoff]
+y2=[-1*fallCutoff,-1*fallCutoff,-1*fallCutoff]
+fig1=plt.figure()
+plt.plot(der, 'k', x,y1,'b',x,y2,'b')
+plt.xlabel('Time')
+plt.ylabel('Derivative of the Number of Photons')
+plt.title('Derivative of # Photons vs. Time')
+plt.grid(True)
+plt.draw()
+plt.show()
+riseCutoff= int(raw_input('What value of the derivative should define a rising edge? 10000? '))
+fallCutoff= int(raw_input('What value of the derivative should define a falling edge? 10000? '))
+plt.close()
 
+plt.close()
+fig0=plt.figure()
+plt.plot(photonSec)
+plt.xlabel('Time')
+plt.ylabel('Number of Photons')
+plt.title('# Photons vs. Time')
+plt.grid(True)
+plt.draw()
+plt.show()
 pause= raw_input("Is there a pause in the middle of the data? Y or press enter ")
 
 if pause in ('Y', "'Y'",'y',"'y'"):
@@ -155,44 +195,56 @@ else:
  
 indi = []
 #print pauseInitial-20
-for g in xrange(0, len(der)):
-   if abs(der[g]) >= ncutoff :
-        indi.append(g)
-#remove close indices that correspond to the same peak
-#print indi
-for i in indi:
-    x = indi.index(i)
-    if x+1 != len(indi) :
-        if abs(indi[x+1]-indi[x]) <= 5 :
-            del indi[x+1]
-x=0
-while x!= len(indi):
-    if indi[x]>(pauseInitial-20):
-#        print indi[x]
-        del indi[x]
-    else:
-    	x=x+1
-#print indi
-if len(indi)%2 != 0:
-    del indi[len(indi)-1]
+risingEdge = []
+fallingEdge = []
+for t in xrange(0,len(der)):
+    if der[t] >= riseCutoff:
+        risingEdge.append(t)
+    if der[t] <= -fallCutoff:
+        fallingEdge.append(t)
 
-   # +- 3 so we don't average the first or last 3 sec in each peak
-init = indi[0] + 3
-numon = len(indi)//2
-   
-spw = []
-stw = []
-
-for j in xrange(0,numon):
-    pw = indi[2*j+1]-indi[2*j]
-    spw.append(pw)
-for k in xrange(0,numon-1):
-    tw = indi[2*(k+1)]-indi[2*k+1]
-    stw.append(tw)
-#print spw
-#print stw
-peakw = np.average(spw) - 2*3
-troughw = np.average(stw) + 2*3
+print risingEdge
+print fallingEdge
+peakw = fallingEdge[0]-risingEdge[0]-10
+troughw = risingEdge[1] - risingEdge[0]-10
+#for g in xrange(0, len(der)):
+#   if abs(der[g]) >= ncutoff :
+#        indi.append(g)
+##remove close indices that correspond to the same peak
+##print indi
+#for i in indi:
+#    x = indi.index(i)
+#    if x+1 != len(indi) :
+#        if abs(indi[x+1]-indi[x]) <= 5 :
+#            del indi[x+1]
+#x=0
+#while x!= len(indi):
+#    if indi[x]>(pauseInitial-20):
+##        print indi[x]
+#        del indi[x]
+#    else:
+#    	x=x+1
+##print indi
+#if len(indi)%2 != 0:
+#    del indi[len(indi)-1]
+#
+#   # +- 3 so we don't average the first or last 3 sec in each peak
+#init = indi[0] + 3
+#numon = len(indi)//2
+#   
+#spw = []
+#stw = []
+#
+#for j in xrange(0,numon):
+#    pw = indi[2*j+1]-indi[2*j]
+#    spw.append(pw)
+#for k in xrange(0,numon-1):
+#    tw = indi[2*(k+1)]-indi[2*k+1]
+#    stw.append(tw)
+##print spw
+##print stw
+#peakw = np.average(spw) - 2*3
+#troughw = np.average(stw) + 2*3
 print 'peakw = %f'%peakw
 print 'troughw = %f'%troughw
 
@@ -234,13 +286,13 @@ if status not in ('Q', 'q', "'Q'", "'q'"):
 #        dectv=dectvManual
 #        wvl=wvlManual
 #    else:
-    testbedFile = np.loadtxt(testbedFileName)
     wvl = testbedFile[:,0]
     dectv=testbedFile[:,-1]
 
     status='R'
     LowPhotonLimit=0
     HighPhotonLimit=0
+    plt.close()
     while status in ('R', "'R'", 'r', "'r'"):
         LowPhotonLimit=0
         HighPhotonLimit=0
@@ -275,7 +327,6 @@ if status not in ('Q', 'q', "'Q'", "'q'"):
         print NPulses[0]
 
         fig2=plt.figure()
-        ax2=fig2.add_subplot(111)
 
 
         qeresults=[]
@@ -293,7 +344,7 @@ if status not in ('Q', 'q', "'Q'", "'q'"):
                     qewvl=(NPulses[j][i]*areaDetector**2)/((dectv[i]*10**7)*(magnification*areaARCONS)**2)
 #                print qewvl
                 qepix.append(qewvl)
-            ax2.plot(wvl,qepix,label='r%dp%d'%(roach,pixlist[j]))
+            plt.plot(wvl,qepix,label='r%dp%d'%(roach,pixlist[j]))
             qeresults.append(qepix)
         qeresults = np.array(qeresults)
         print 'qeresult[0]=',qeresults[0]
@@ -303,35 +354,34 @@ if status not in ('Q', 'q', "'Q'", "'q'"):
         print 'medianQE=',medianQeResults
         #print meanQeResults
 #-------Some error above.  inputs right, formula right, output not right...------
-        ax2.set_xlabel('Frequency (nm)')
-        ax2.set_ylabel('QE')
-        ax2.set_title('QE of %s in r%d'%(qeDataFilename,roach))
-        ax2.grid(True)
-        ax2.legend()
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('QE')
+        plt.title('QE of %s in r%d'%(qeDataFilename,roach))
+        plt.grid(True)
+        plt.legend()
+        plt.draw()
         plt.show()
-        plt.close()
 #        print 'The decimal quantum efficiency of the ADR for 400-1050 wvls are:'
 #        print qeresults
 
         Save= raw_input("Do you want to save this next plot of the median QE vs. wavelength? Y or enter: ")
-
-        fig3=plt.figure()
-        ax3=fig3.add_subplot(111)
-#        ax3.plot(wvl,medianQeResults)
-        ax3.plot(wvl,medianQeResults, label='median')
-        ax3.plot(wvl,meanQeResults, label='mean')
-        ax3.set_xlabel('Frequency (nm)')
-        ax3.set_ylabel('QE')
-        ax3.legend()
-        ax3.set_title('Median and Mean QE of %s in r%d'%(qeDataFilename,roach), size=12)
-        ax3.grid(True)
-        if Save in ('Y', "'Y'",'y',"'y'"):
-            okName= raw_input("Is %s an ok filename? N or enter: "%(saveMedQEPlotname))
-            if okName in ('N', "'N'", 'n', "'n'"):
-                saveMedQEPlotname= raw_input('Enter the desired filename: ')
-            plt.savefig(saveMedQEPlotname)
-        plt.show()
         plt.close()
 
+        fig3=plt.figure()
+        plt.plot(wvl,medianQeResults, label='median')
+        plt.plot(wvl,meanQeResults, label='mean')
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('QE')
+        plt.legend()
+        plt.title('Median and Mean QE of %s in r%d'%(qeDataFilename,roach), size=12)
+        plt.grid(True)
+        if Save in ('Y', "'Y'",'y',"'y'"):
+            saveMedQEPlotname= raw_input('Enter the desired filename: ')
+            print saveMedQEPlotname
+            plt.savefig(saveMedQEPlotname)
+        plt.draw()
+        plt.show()
+
         status = raw_input("What do you want to do? \nRepeat with different pixels= R or press enter to finish: ")
+        plt.close()
 
