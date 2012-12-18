fd9a09d91c433c8fb7f3e061cf5b7afd03615181
Matt Strader <mstrader@physics.ucsb.edu>
2012-12-18 11:57:33 -0800

    Added commented section to incorporate smart filling for x-only or y-only positions

--- Setup/Beammapping/TxtToBeamImg.py
+++ Setup/Beammapping/TxtToBeamImg.py
@@ -97,3 +97,87 @@ print overlaps, 'Overlapping pixels'
 #        if ca[row,col] == '':
 #            ca[row,col] = noloc[iEmptyPix]
 #            iEmptyPix += 1
+
+#smartfill = False
+#if smartfill == True:
+#    xOnlyPosList = np.recfromtxt(path+'good_x_right.txt')
+#    xOnlyFreqList = xOnlyPosList['f0']
+#    xOnlyAttenList = xOnlyPosList['f1']
+#    xOnlyXList = xOnlyPosList['f2']
+#    xOnlyPixelNames = xOnlyPosList['f4']
+#
+#    yOnlyPosList = np.recfromtxt(path+'good_y_right.txt')
+#    yOnlyFreqList = yOnlyPosList['f0']
+#    yOnlyAttenList = yOnlyPosList['f1']
+#    yOnlyYList = yOnlyPosList['f3']
+#    yOnlyPixelNames = yOnlyPosList['f4']
+#
+#print ''
+#print 'now x only pixels'
+#for iPos,pixelName in enumerate(xOnlyPixelNames):
+#    x = xOnlyXList[iPos]
+#    freq = xOnlyFreqList[iPos]
+#    attenval = xOnlyFreqList[iPos]
+#    print x,pixelName,
+#    if x >=0 and x < nCol:
+#        possibleY = np.where(ca[:,x] == '')[0]
+#        if len(possibleY) > 0:
+#            y = possibleY[0]
+#            ca[y,x] = pixelName
+#            atten[y,x] = attenval
+#            resfreq[y,x] = freq
+#            h5file.flush()
+#            print 'placed at ',y
+#        else:
+#            print ''
+#            print 'Nowhere at x to place it'
+#            overlaps += 1
+#            noloc.append(pixelName)
+#    else:
+#        print ''
+#        print 'Pixel location out of bounds'
+#        noloc.append(pixelName)
+#
+#
+#print 'now y only pixels'
+#for iPos,pixelName in enumerate(yOnlyPixelNames):
+#    y = yOnlyYList[iPos]
+#    freq = yOnlyFreqList[iPos]
+#    attenval = yOnlyFreqList[iPos]
+#    print y,pixelName,
+#    if y >=0 and y < nRow:
+#        possibleX = np.where(ca[y,:] == '')[0]
+#        if len(possibleX) > 0:
+#            x = possibleX[0]
+#            ca[y,x] = pixelName
+#            atten[y,x] = attenval
+#            resfreq[y,x] = freq
+#            h5file.flush()
+#            print 'placed at ',x
+#        else:
+#            print ''
+#            print 'Nowhere at y to place it'
+#            overlaps += 1
+#            noloc.append(pixelName)
+#    else:
+#        print ''
+#        print 'Pixel location out of bounds'
+#        noloc.append(pixelName)
+##fill the rest in randomly
+#for iEmptyPix,pixelName in enumerate(noloc):
+#    row = random.randint(0,45)
+#    col = random.randint(0,43)
+#    while (ca[row,col] != ''):
+#        row = random.randint(0,45)
+#        col = random.randint(0,43)
+#    if placeUnbeammappedPixels == 0:
+#        pixelName = '/r0/p250/'
+#    ca[row,col] = pixelName
+#
+#h5file.flush()
+#h5file.close()
+#print len(pixelNames),'good pixels'
+#print len(xOnlyPixelNames),'x only pixels'
+#print len(yOnlyPixelNames),'y only pixels'
+#print len(noloc),'Unbeammpped pixels'
+#print overlaps, 'Overlapping pixels'
