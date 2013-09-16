41c13312b04ffa90d6f717ba008610281b53f8ea
mstrader <mstrader@physics.ucsb.edu>
2013-09-16 09:03:28 -0700

    simulates baseline lpf faster with FIR

--- Projects/Simulator/simulateTriggers.py
+++ Projects/Simulator/simulateTriggers.py
@@ -1,4 +1,7 @@
 from matplotlib import rcParams, rc
+import numpy as np
+import sys
+from fitFunctions import gaussian
 
 # common setup for matplotlib
 params = {'savefig.dpi': 300, # save figures to 300 dpi
@@ -19,8 +22,8 @@ import numpy as np
 import os
 import struct
 
-def calcThreshold(phase,Nsigma=2.5):
-    n,bins= np.histogram(phase,bins=100)
+def calcThreshold(phase,Nsigma=2.5,nSamples=5000):
+    n,bins= np.histogram(phase[:nSamples],bins=100)
     n = np.array(n,dtype='float32')/np.sum(n)
     tot = np.zeros(len(bins))
     for i in xrange(len(bins)):
@@ -36,6 +39,18 @@ def IIR(x,t,iters=0):
     else:
         return 0.08*x[t-30]+0.92*IIR(x,t-10,iters=iters+1)
 
+def lpfFIR(x,nIters=100):
+    firCoeff=0.08
+    iirCoeff=1-firCoeff
+    neighborJump = 10
+    y = np.roll(x,30)
+    for i in np.arange(1,nIters):
+        y += (iirCoeff**i)*np.roll(x,30+neighborJump*i)
+    y = y*firCoeff
+    return y
+        
+    
+
 def tryFilter(filter,sample,fig=None,NAxes=1,iAxes=1,label=''):
     filtered = np.correlate(filter,sample,mode='same')[::-1]
     baselines=np.array([IIR(filtered,t) for t in xrange(len(filtered))])
@@ -48,11 +63,10 @@ def tryFilter(filter,sample,fig=None,NAxes=1,iAxes=1,label=''):
         ax.plot([0,len(sample)-1],[threshold,threshold])
         ax.set_title(label)
 
-def detectPulses(sample,threshold):
+def detectPulses(sample,threshold,baselines):
     filtered = np.array(sample)
 
     #threshold = calcThreshold(filtered[0:2000])
-    baselines=np.array([IIR(filtered,t) for t in xrange(len(filtered))])
     filtered -= baselines
     derivative = np.diff(filtered)
     peakHeights = []
@@ -67,8 +81,9 @@ def detectPulses(sample,threshold):
     peakIndices = np.where(peakBooleans)[0]+1
     i = 0
     p = peakIndices[i]
+    deadtime=0#100#us
     while p < peakIndices[-1]:
-        peakIndices = peakIndices[np.logical_or(peakIndices-p > 100 , peakIndices-p <= 0)]
+        peakIndices = peakIndices[np.logical_or(peakIndices-p > deadtime , peakIndices-p <= 0)]#apply deadtime
         i+=1
         if i < len(peakIndices):
             p = peakIndices[i]
@@ -80,27 +95,35 @@ def detectPulses(sample,threshold):
     peakBaselines = baselines[peakIndices]
     return peakIndices,peakHeights,peakBaselines
 
-roachNum = 0
-pixelNum = 136
-secs=5
-#folder = '/Scratch/filterData/20121204/'
-folder = '/Scratch/newArrayData/20130220/'
-cps=200
-bFiltered = True
+rootFolder = '/home/kids/labData/'
 
+#roachNum = 0
+#pixelNum = 136
+#secs=5
+#folder = '/home/kids/labData/20130220/'
+#cps=600
+#bFiltered = True
+#phaseFilename = os.path.join(folder,'ch_snap_r%dp%d_%dsecs_%dcps.dat'%(roachNum,pixelNum,secs,cps))
 
-phaseFilename = os.path.join(folder,'ch_snap_r%dp%d_%dsecs_%dcps.dat'%(roachNum,pixelNum,secs,cps))
+roachNum = 4
+pixelNum = 102
+secs=50
+folder = os.path.join(rootFolder,'20121204/')
+cps=600
+bFiltered = False
+phaseFilename = os.path.join(folder,'ch_snap_r%dp%d_%dsecs.dat'%(roachNum,pixelNum,secs))
+
+bPlotPeaks = False
+
 phaseFile = open(phaseFilename,'r')
 phase = phaseFile.read()
 numQDRSamples=2**19
 numBytesPerSample=4
 nLongsnapSamples = numQDRSamples*2*secs
-qdr_values = struct.unpack('>%dh'%(nLongsnapSamples),phase)
-qdr_phase_values = np.array(qdr_values,dtype=np.float32)*360./2**16*4/np.pi #convert from adc units to degrees
-print len(qdr_phase_values),'us'
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(qdr_phase_values)
+qdrValues = struct.unpack('>%dh'%(nLongsnapSamples),phase)
+qdrPhaseValues = np.array(qdrValues,dtype=np.float32)*360./2**16*4/np.pi #convert from adc units to degrees
+nPhaseValues=len(qdrValues)
+print nPhaseValues,'us'
 
 fig = plt.figure()
 NAxes = 1
@@ -109,51 +132,62 @@ size=26
 offset = 3
 
 sampleStart = 5000
-nSamples = 20000
+nSamples = nPhaseValues-sampleStart
 thresholdLength = 2000
 thresholdSigma = 2.5
 
-sample=qdr_phase_values[sampleStart:sampleStart+nSamples]
-unfiltered = np.array(sample)
+sample=qdrValues[sampleStart:sampleStart+nSamples]
+#sample = np.array(qdrPhaseValues)
 if bFiltered == False:
-    filter= np.loadtxt('/Scratch/filterData/fir/template20121207r%d.txt'%roachNum)[pixelNum,:]
-    lpf250kHz= np.loadtxt('/Scratch/filterData/fir/lpf_250kHz.txt')
+    unfiltered = np.array(sample)
+    filter= np.loadtxt(os.path.join(rootFolder,'fir/template20121207r%d.txt'%roachNum))[pixelNum,:]
+    #lpf250kHz= np.loadtxt('/Scratch/filterData/fir/lpf_250kHz.txt')
     filtered = np.correlate(filter,unfiltered,mode='same')[::-1]
-    lpffiltered = np.correlate(lpf250kHz,unfiltered,mode='same')[::-1]
+    #lpffiltered = np.correlate(lpf250kHz,unfiltered,mode='same')[::-1]
+    print 'filtering done'
+    sys.stdout.flush()
 else:
-    filtered = np.array(unfiltered)
-baselines=np.array([IIR(filtered,t) for t in xrange(len(filtered))])
+    filtered = np.array(sample)
+
+baselines=np.array(lpfFIR(filtered),dtype=np.int)
+print 'baselines done'
+sys.stdout.flush()
+#baselines2=np.array([IIR(filtered,t) for t in xrange(len(filtered))])
+#print 'baselines2 done'
+sys.stdout.flush()
 threshold = calcThreshold(filtered[0:thresholdLength],Nsigma=thresholdSigma)
+print 'threshold done'
+sys.stdout.flush()
 
-#ax=fig.add_subplot(NAxes,1,iAxes)
-#ax.plot(unfiltered,'k.-',label='unfiltered phase')
-#ax.set_ylabel('unfiltered phase (${}^{\circ}$)')
-#ax.set_xlim([5000,15000])
-#iAxes+=1
-
-#ax=fig.add_subplot(NAxes,1,iAxes)
-#ax.plot(lpffiltered,'k',label='250kHz lpf filtered phase')
-#ax.set_ylabel('250kHz lpf phase (${}^{\circ}$)')
-#ax.set_xlim([5000,15000])
-#iAxes+=1
-
-ax=fig.add_subplot(NAxes,1,iAxes)
-ax.plot(filtered,'k.-',label='optimal filtered phase')
-ax.plot(baselines,'b',label='lpf baseline')
-ax.plot(baselines+threshold,'y--',label='threshold')
-idx,peaks,bases = detectPulses(filtered,threshold)
+idx,peaks,bases = detectPulses(filtered,threshold,baselines)
 idx+=sampleStart
-ax.plot(idx-sampleStart,peaks+bases,'ro',label='detected peak')
-ax.plot(idx-sampleStart,bases,'go',label='detected baseline')
-ax.set_xlabel('time (us)')
-ax.set_ylabel('phase (${}^{\circ}$)')
-ax.set_xlim([5000,15000])
-#ax.set_title('detected peaks and baseline for ~%d cps, pixel /r%d/p%d'%(cps,roachNum,pixelNum))
-ax.legend(loc='lower right')
-iAxes+=1
-outFilename = os.path.splitext(phaseFilename)[0]+'.png'
-#plt.savefig(outFilename)
+print len(peaks),'peaks detected'
+sys.stdout.flush()
 
+
+if bPlotPeaks:
+    ax=fig.add_subplot(NAxes,1,iAxes)
+    ax.plot(filtered,'k.-',label='optimal filtered phase')
+    ax.plot(baselines,'b',label='lpf baseline')
+    #ax.plot(baselines2,'c--',label='lpf baseline')
+    ax.plot(baselines+threshold,'y--',label='threshold')
+
+    ax.plot(idx-sampleStart,peaks+bases,'r.',label='detected peak')
+    ax.plot(idx-sampleStart,bases,'g.',label='detected baseline')
+    ax.set_xlabel('time (us)')
+    ax.set_ylabel('phase (${}^{\circ}$)')
+    #ax.set_xlim([5000,15000])
+    #ax.set_title('detected peaks and baseline for ~%d cps, pixel /r%d/p%d'%(cps,roachNum,pixelNum))
+    ax.legend(loc='lower right')
+    iAxes+=1
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(qdrValues[::100],'.')
+energies = peaks-bases
+
+np.savez('detectedRed.npz',idx=idx,peaks=peaks,bases=bases,energies=energies,baselines=baselines,threshold=threshold,qdrValues=qdrValues,filtered=filtered)
 print 'done'
+sys.stdout.flush()
 plt.show()
 
