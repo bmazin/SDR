f67758546e781b3b679bafd1580977f597659fbf
Matt Strader <mstrader@physics.ucsb.edu>
2013-11-14 19:03:59 -0800

    added more firmware options with DDS lags

--- DataReadout/ChannelizerControls/startSeqFirmware.sh
+++ DataReadout/ChannelizerControls/startSeqFirmware.sh
@@ -4,7 +4,10 @@ CLK="512"
 #BOF="chan_if_acc_x_2011_Aug_02_0713.bof" #Palomar 2011 firmware,has deadtime lockout bug, 3-pt trigger bug
 #BOF="chan_dtrig_v2_2012_Aug_28_1956.bof" #Has short low pass filter baseline, plus Palomar 2011 bugs
 #BOF="btrig_fix_v1_2012_Sep_06_1516.bof" *fixed deadtime lockout, longer low pass filter baseline
-BOF="chan_snap_v3_2012_Oct_30_1216.bof" #has snapQDR block for long snapshots
+BOF="chan_snap_v3_2012_Oct_30_1216.bof" #has snapQDR block for long snapshots, DDS lag: 154
+#BOF="chan_snap_v3_2013_Oct_03_1412.bof" #has snapQDR block for long snapshots, DDS lag: 192
+#BOF="chan_dead_v2_2013_Oct_07_1841.bof" #short dead time + count rate limiter, DDS lag: 194
+#BOF="chan_smart_v1_2013_Nov_13_1403.bof" #has moving average baseline block. DDS lag:
 
 if [ "$1" == "--help" ]; then
 	echo "Usage: $0 CLOCK_MHZ [BOF_FILE]"
