aeb14de33687f32b6a88d49848f66ed8e4cc5912
Matt Strader <mstrader@physics.ucsb.edu>
2013-12-04 15:10:15 -0800

    now script won't stop if it has an problem on one roach.
    It simply prints an error and tries the next roach

--- DataReadout/ChannelizerControls/lib/set_alpha.py
+++ DataReadout/ChannelizerControls/lib/set_alpha.py
@@ -9,6 +9,10 @@ roach = []
 alpha = 0.08
 out=castBin(alpha,quantization='Round')
 for i in xrange(8):
+    print 'roach ',i
     roach = corr.katcp_wrapper.FpgaClient('10.0.0.1%d'%i,7147)
     time.sleep(.2)
-    roach.write_int('capture_Baseline_alpha',out)
+    try:
+        roach.write_int('capture_Baseline_alpha',out)
+    except:
+        print 'ERROR writing alpha register'
