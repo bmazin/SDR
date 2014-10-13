a046d11db213d2f1152d35716f61e2297d424135
Chris Stoughton <chris.stoughton@gmail.com>
2014-10-13 15:27:10 -0500

    use MKIDS_ROACHES to determine list of roach boards in use

--- DataReadout/ChannelizerControls/lib/set_alpha.py
+++ DataReadout/ChannelizerControls/lib/set_alpha.py
@@ -3,14 +3,15 @@ import corr
 import time
 import struct
 import sys
+import os
 from Utils.bin import *
 
 roach = []
 alpha = 0.08
 out=castBin(alpha,quantization='Round')
-for i in xrange(8):
-    print 'roach ',i
-    roach = corr.katcp_wrapper.FpgaClient('10.0.0.1%d'%i,7147)
+for i in os.environ['MKID_ROACHES']:
+    print 'in set_alpha.py:  roach ',i
+    roach = corr.katcp_wrapper.FpgaClient('10.0.0.1%s'%i,7147)
     time.sleep(.2)
     try:
         roach.write_int('capture_Baseline_alpha',out)
