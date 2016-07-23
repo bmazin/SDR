8b7308a641f05505c9119f23bc5aabefeae5bcf6
abwalter <abwalter@physics.ucsb.edu>
2016-07-22 20:29:08 -0700

    now include pos sideband in beammap

--- Projects/BestBeammap/moveOverlappingPixels.py
+++ Projects/BestBeammap/moveOverlappingPixels.py
@@ -5,12 +5,12 @@ import os
 
 basePath = '/mnt/data0/Darkness/20160721/'
 if __name__=='__main__':
-    labels = ['fl1 neg','fl2 neg','fl3 neg']
-    feedlines = [1,2,3]
+    labels = ['fl1 neg','fl1 pos','fl2 neg','fl2 pos','fl3 neg','fl3 pos']
+    feedlines = [1,1,2,2,3,3]
 
-    filenames = ['beamlist_FL1_neg.txt','beamlist_FL2_neg.txt','beamlist_FL3_neg.txt']
+    filenames = ['beamlist_FL1_neg.txt','beamlist_FL1_pos.txt','beamlist_FL2_neg.txt','beamlist_FL2_pos.txt','beamlist_FL3_neg.txt','beamlist_FL3_pos.txt']
     colors = ['blue','cyan','red','darkred','darkblue','green','magenta']
-    yOffsets = [0,25,50]
+    yOffsets = [0,0,25,25,50,50]
 
     nRows = 75
     nCols = 80
@@ -25,12 +25,14 @@ if __name__=='__main__':
     for iLabel,(label,filename) in enumerate(zip(labels,filenames)):
         segmentLocationData = locationData[iLabel]
         yOffset = yOffsets[iLabel]
+        fails = np.array(segmentLocationData[:,1],dtype=np.bool)
         xs = np.floor(segmentLocationData[:,2])
         ys = np.floor(segmentLocationData[:,3]) + yOffset
         preciseXs = segmentLocationData[:,2]
-        preciseYs = segmentLocationData[:,3]
-        ax.scatter(xs,ys,label=label,color=colors[iLabel])
-        for (x,y) in zip(xs,ys):
+        preciseYs = segmentLocationData[:,3]+yOffset
+        goodMask = (~fails) & (preciseXs != -1)
+        ax.scatter(xs[goodMask],ys[goodMask],label=label,color=colors[iLabel])
+        for (x,y) in zip(xs[goodMask],ys[goodMask]):
             if 0 <= x and x < nCols and 0 <= y and y < nRows:
                 if grid[y,x] != 0:
                     grid[y,x] = 10
@@ -47,17 +49,22 @@ if __name__=='__main__':
         print iLabel
         yOffset = yOffsets[iLabel]
         resIds = locationData[iLabel][:,0]
-        failedBeammaps = locationData[iLabel][:,1]
         xs = np.floor(locationData[iLabel][:,2])
         ys = np.floor(locationData[iLabel][:,3]) + yOffset
         ys[xs==-1] = -1
         preciseXs = locationData[iLabel][:,2]
-        preciseYs = locationData[iLabel][:,3]
+        preciseYs = locationData[iLabel][:,3]+yOffset
+        fails = np.array(locationData[iLabel][:,1],dtype=np.bool)
+        goodMask = (~fails) & (preciseXs != -1)
+        print 'good',np.sum(goodMask)
+        print 'res',np.min(resIds[goodMask]),np.max(resIds[goodMask])
+        
         for i in range(len(locationData[iLabel])):
             resId = resIds[i]
-            distVector = (preciseXs[i]-(xs[i]+.5),preciseYs[i]-(ys[i]+.5))
-            distMag = np.sqrt(distVector[0]**2 + distVector[1]**2)
-            gridDicts.append({'x':xs[i],'y':ys[i],'preciseX':preciseXs[i],'preciseY':preciseYs[i],'distMag':distMag,'segment':iLabel,'resId':resId})
+            if goodMask[i]:
+                distVector = (preciseXs[i]-(xs[i]+.5),preciseYs[i]-(ys[i]+.5))
+                distMag = np.sqrt(distVector[0]**2 + distVector[1]**2)
+                gridDicts.append({'x':xs[i],'y':ys[i],'preciseX':preciseXs[i],'preciseY':preciseYs[i],'distMag':distMag,'segment':iLabel,'resId':resId,'feedline':feedlines[iLabel],'fail':0})
 
     gridDicts = np.array(gridDicts)
 
@@ -85,8 +92,9 @@ if __name__=='__main__':
                             newX = unassignedNeighbors[bestUnassignedNeighbor][0]
                             newY = unassignedNeighbors[bestUnassignedNeighbor][1]
                             print 'best unassigned neighbor',bestUnassignedNeighbor,newX,newY
-                            #gridDicts[gridIndices[j]]['x'] = newX
-                            #gridDicts[gridIndices[j]]['y'] = newY
+                            gridDicts[gridIndices[j]]['x'] = newX
+                            gridDicts[gridIndices[j]]['y'] = newY
+                            grid[newY,newX] = 7
                         except IndexError:
                             print 'no neighbor could be found'
                         except:
@@ -95,17 +103,11 @@ if __name__=='__main__':
     plotArray(grid,origin='upper')
     plt.show()
     print 'next'
-    newLocationData = np.concatenate(locationData)
 
-    for i in range(len(newLocationData)):
-        gridEntry = [item for item in gridDicts if item['resId']==newLocationData[i][0]]
-        print newLocationData[i]
-        gridEntry = gridEntry[0]
-        newLocationData[i,2] = gridEntry['x']
-        newLocationData[i,3] = gridEntry['y']
-        print newLocationData[i]
+    newLocationData = [[entry['resId'],entry['fail'],entry['x'],entry['y'],entry['feedline']] for entry in gridDicts]
+    newLocationData = np.array(newLocationData)
         
-    np.savetxt('adjusted_beammap.txt',newLocationData,fmt='%d %d %d %d')
+    np.savetxt('adjusted_beammap.txt',newLocationData,fmt='%d\t%d\t%d\t%d\t%d')
     print 'done!'
 
 
